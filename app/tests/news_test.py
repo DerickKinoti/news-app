af2af8a5eb60c47745c76b706be8e1641bcd2bd5
DerickKinoti <68073216+DerickKinoti@users.noreply.github.com>
2020-09-11 21:14:45 +0300

    Update news_test.py
    second commit

--- app/tests/news_test.py
+++ app/tests/news_test.py
@@ -3,10 +3,8 @@ from app.models import news
 News = news.News
 
 class newsTest(unittest.TestCase):
-
     def setUp(self):
         self.newsArticle= News('business-insider','ddd','http://www.businessinsider.com','kkk')
-
-
-    def test_instance(self):
+        
+     def test_instance(self):
         self.assertIsInstance(self.newsArticle,News)
